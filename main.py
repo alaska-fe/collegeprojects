1e2087989d3ad09da7b58bd690a21af1ec10d991
timoshka <bondarenkotim08@gmail.com>
2026-09-04 13:28:18 +0300

    Update main.py
    windows compatibility with weter

--- main.py
+++ main.py
@@ -1,13 +1,16 @@
-import datetime, os, platform, pwd
+import datetime
+import os
+import platform
+import psutil
 
-def sbor_info_pc():
+def sbor_info_pc(humidity_param, wind_param):
     try:
         username = os.getlogin()
     except OSError:
-        username = os.getenv('USER') or os.getenv('LOGNAME') or 'unknown'
-
-    pc_data = f'{username}-{platform.node()}-{platform.processor()}'
+        username = os.getenv('USERNAME') or os.getenv('USER') or os.getenv('LOGNAME') or 'unknown'
+    pc_data = f'{username}-{platform.node()}-{platform.processor()}-{humidity_param}-{wind_param}'
     return abs(hash(pc_data))
+
 
 def random_gen(first, second):
     if not isinstance(first, int) or not isinstance(second, int):
@@ -17,12 +20,18 @@ def random_gen(first, second):
     if first > second:
         first, second = second, first
 
-    time_seed = int(str(datetime.datetime.now())[-6:])
-    hw_seed = sbor_info_pc()
+    humidity_param = psutil.virtual_memory().percent
+    wind_param = psutil.cpu_percent(interval=None)
+
+    time_seed = datetime.datetime.now().microsecond
+    hw_seed = sbor_info_pc(humidity_param, wind_param)
+
     combined_seed = time_seed ^ hw_seed
     dynamic_range = second - first + 1
-    pseudo_random = (combined_seed * 1103515245 + 12345) % 2 ** 31
+    pseudo_random = (combined_seed * 1103515245 + 12345) % (2 ** 31)
     result = first + (pseudo_random % dynamic_range)
     return result
 
-print(random_gen(2, 10000))
+
+# Выведет только число-результат
+print(random_gen(2, 4))
